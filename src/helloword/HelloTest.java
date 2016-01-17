26ca50006b3fe3f5717f5cccfd869d5316a7e445
yuqiuyuan <derbander@163.com>
2016-01-17 14:55:57 +0800

    Summary
    Description test info .

---
+++ src/helloword/HelloTest.java
@@ -0,0 +1,7 @@
+package helloword;
+
+public class HelloTest {
+	public static void main(String[] args) {
+		System.out.println("安一个快捷键都这么慢！");
+	}
+}
---
+++ src/helloword/HelloWord.java
@@ -0,0 +1,13 @@
+package helloword;
+/**
+ * first program in Mac
+ * @author yuqiuyuan
+ *
+ */
+public class HelloWord {
+	public static void main(String[] args) {
+		System.out.println("Hello world !");
+		System.out.println("使用电脑设置eclipse的时候会遇到卡顿，解决方案是强制关闭！");
+	}
+
+}
